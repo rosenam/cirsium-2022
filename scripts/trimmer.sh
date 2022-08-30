a0e7776f7b42a4e177131e0bc0f0f2265fde3f9e
rosenam <austin.rosen1@gmail.com>
2022-08-30 08:55:48 -0600

    Update trimmer.sh

--- scripts/trimmer.sh
+++ scripts/trimmer.sh
@@ -7,15 +7,17 @@
 #SBATCH --time=168:00:00   # modify this to reflect how long to let the job go. 
 #SBATCH --output=trimmer_log_%j.txt
 
-module load jdk
 
+module load jdk                 #load java module
 
 for fileR1 in *R1.fastq.gz       #run trimmomatic for each pair of files
 do
-fileR2=`echo ${fileR1} | sed 's/R1/R2/'`
-fwdpair=`echo ${fileR1} | sed 's/R1.fastq.gz/fwd_paired.fastq/'`
+fileR2=`echo ${fileR1} | sed 's/R1/R2/'`                                  #name fileR2 by swapping R1 with R2
+fwdpair=`echo ${fileR1} | sed 's/R1.fastq.gz/fwd_paired.fastq/'`          #give each file a more informative name
 revpair=`echo ${fileR2} | sed 's/R2.fastq.gz/rev_paired.fastq/'`
 fwdunp=`echo ${fileR1} | sed 's/R1.fastq.gz/fwd_unpaired.fastq/'`
 revunp=`echo ${fileR2} | sed 's/R2.fastq.gz/rev_unpaired.fastq/'`
+
+#run trimmomatic with the correct adapter file and trimming options of your choice
 java -jar trimmomatic-0.39.jar PE $fileR1 $fileR2 $fwdpair $fwdunp $revpair $revunp ILLUMINACLIP:/projects/cirsium/01_input/TruSeq3-PE-2.fa:2:30:10 LEADING:20 TRAILING:20 SLIDINGWINDOW:5:20 MINLEN:36
 done
